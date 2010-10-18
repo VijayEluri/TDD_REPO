30eeeb7c277facdf5741aa180243f31f9a0e508c
mihaela manigutiu <cam_miha1@yahoo.com>
2010-10-18 20:48:47 +0300

    create a test class

---
+++ src/com/tdd/Test.java
@@ -0,0 +1,11 @@
+package com.tdd;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: miha
+ * Date: Oct 18, 2010
+ * Time: 8:48:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
